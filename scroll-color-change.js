f4b39f2fd4f5ae0e4a263144428c79b8665b212e
Sotiris Kakanos <sotiris@automata.tech>
2024-09-07 22:51:41 +0100

    simplfied js

--- scroll-color-change.js
+++ scroll-color-change.js
@@ -1,38 +1,22 @@
+var buttonLinks = document.querySelectorAll('.button a');
+var logo = document.querySelector('.logo');
+
 window.addEventListener('scroll', function() {
     var navBar = document.getElementById('nav-bar');
-    if (navBar) { // Check if the div actually exists
-        if (window.scrollY > 100) {
-            navBar.style.color = "black"
-            navBar.style.backgroundColor = 'white';
-        } else {
-            navBar.style.color = "white"
-            navBar.style.backgroundColor = 'transparent';
-        }
+    var textColor = window.scrollY > 100 ? 'black' : 'white';
+    var backgroundColor = window.scrollY > 100 ? 'white' : 'transparent';
+
+    if (navBar) {
+        navBar.style.color = textColor;
+        navBar.style.backgroundColor = backgroundColor;
     } else {
         console.error('Element with ID "nav-bar" not found.');
     }
-});
 
-var buttonLinks = document.querySelectorAll('.button a');
-var logo = document.querySelector('.logo');
-buttonLinks.forEach(function(link) {
-    link.style.color = 'white'; // Example: changing the text color of all links
-});
-
-window.addEventListener('scroll', function() {
-    if (buttonLinks) { // Check if the div actually exists
-        if (window.scrollY > 100) {
-            logo.style.color = 'black';
-            buttonLinks.forEach(function(link) {
-                link.style.color = 'black';
-            });
-        } else {
-            logo.style.color = 'white';
-            buttonLinks.forEach(function(link) {
-                link.style.color = 'white';
-            });
-        }
-    } else {
-        console.error('Element with ID "nav-bar" not found.');
+    if (buttonLinks) {
+        logo.style.color = textColor;
+        buttonLinks.forEach(function(link) {
+            link.style.color = textColor;
+        });
     }
 });
