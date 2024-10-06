0aa477bcf61a11399b207a5e9961bcab6ea1f9c0
Sotiris Kakanos <sotiris@automata.tech>
2024-10-06 12:41:31 +0100

    chore: using pre-written js for on scroll animations

--- js/fade-in-logos.js
+++ js/fade-in-logos.js
@@ -1,52 +1,15 @@
-const parentElement = document.querySelector('.client-logos');
-
-// list child elements
-const childElements = Array.from(parentElement.children);
-
-
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
-  }
-
-function fadeInElement(element, fadeTime) {
-    element.style.opacity = 1;
-    element.style.transition = `opacity ${fadeTime}s`;
-};
-
-// fade in each child element
-function fadeInAll() {
-    childElements.forEach((element) => {
-        let randomTime = getRandomArbitrary(1, 3);
-        let randomDelay = getRandomArbitrary(100, 1000);
-        setTimeout(() => {
-            fadeInElement(element, randomTime);
-        }, randomDelay);
-    });
-};
-
-const firstChildPosition = Math.round(childElements[0].getBoundingClientRect().top);
-window.addEventListener('scroll', function() {
-    // get vertical window size
-    // get the xy position of the first child element
-    
-    let fadeHappened = false;
-    let positionQuery = Math.round(window.scrollY);
-    console.log(positionQuery, firstChildPosition);
-
-    if ((positionQuery == firstChildPosition) && !fadeHappened) {
-        fadeInAll();
-        fadeHappened = true;
+let elementsArray = document.querySelectorAll(".tile");
+console.log(elementsArray);
+window.addEventListener('scroll', fadeIn ); 
+function fadeIn() {
+    for (var i = 0; i < elementsArray.length; i++) {
+        var elem = elementsArray[i]
+        var distInView = elem.getBoundingClientRect().top - window.innerHeight + 20;
+        if (distInView < 0) {
+            elem.classList.add("inView");
+        } else {
+            elem.classList.remove("inView");
+        }
     }
-    else {
-        console.log('nope');
-    }
-
-    // if (positionQuery < firstChildPosition.top && fadeHappened) {
-    //     console.log('reset');
-    //     fadeHappened = false;
-    //     childElements.forEach((element) => {
-    //         element.style.opacity = 0;
-    //     });
-
-    // }
-});
+}
+fadeIn();
